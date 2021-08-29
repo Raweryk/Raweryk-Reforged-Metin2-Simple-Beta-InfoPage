2a8465289806f1596ed54f9cde7d010d3da86f69
Raweryk <raweryk@gmail.com>
2021-08-29 06:58:05 +0200

    small update

---
+++ rwrkInitialDevTestJQueryTranslated_1/content/language.php
@@ -0,0 +1,12 @@
+<?php
+	$serverName = "serverName";
+	$serverHello = "serverHello";
+	$serverDesc = "serverDesc";
+	$discordURI = "https://discord.gg/jzp4gvTEVx";
+	$facebookURI = "https://fb.me/Raweryk";
+	$downloadURI = "#";
+	$buttonDesc = "buttonDesc";//Załóż konto
+//	$buttonURI = "./metin2.exe";
+	$underButtonINFO= "underButtonINFO";//"Posiadasz już konto?";
+	$underButtonURI = "underButtonURI";//"Kliknij, aby pobrać";
+?>
